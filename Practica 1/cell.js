53648cadc28f9c879c200c6a35a747f8bad45a91
AlexCabezas2018 <alexcabezas0705@gmail.com>
2020-02-19 00:44:14 +0100

    practica 1 progreso

--- Practica 1/cell.js
+++ Practica 1/cell.js
@@ -3,10 +3,11 @@ class Cell {
         this.x = x;
         this.y = y;
         this.w = w;
+        this.element = NOTHING;
     }
 
     draw() {
-        fill(255);
+        fill(this.element);
         rect(this.x * this.w, this.y * this.w, this.w, this.w);
     }
 }
--- Practica 1/script.js
+++ Practica 1/script.js
@@ -2,36 +2,58 @@ let board;
 let rowSlider;
 let columnSlider;
 
+let docElemsRow;
+let docElemsCols;
+
 // Constantes
-const DEF_ROWS = 9;
+const DEF_ROWS = 10;
 const DEF_COLS = 10;
 const DEF_SIZE = 40;
 
+// Elementos (los representamos con colores)
+const NOTHING = '#ffffff';
+const BEGIN = '#00ff00';
+const OBSTACLE = '#ff0000';
+const PENALIZATION = '#ffb700';
+const END = '#0000ff';
+const PATH = '#cf91ff';
+
+let currentElement;
+
 function setup() {
-    let canvas = createCanvas(DEF_SIZE * DEF_COLS, DEF_SIZE * DEF_ROWS);
-    canvas.parent('canvas-board');
-    const x = (windowWidth - width) / 2;
-    const y = (windowHeight - height) / 2;
-    canvas.position(x, y);
+    setupCanvas()
 
-    rowSlider = createSlider(3, 25, 10, 1);
-    columnSlider = createSlider(3, 25, 10 ,1);
+    rowSlider = createSlider(3, 25, DEF_ROWS, 1);
+    columnSlider = createSlider(3, 25, DEF_COLS, 1);
 
-    rowSlider.position(20, 50);
-    rowSlider.style('width', '80px');
+    rowSlider.parent('options-section')
+    columnSlider.parent('options-section')
 
-    background(0);
-    board = setupBoard();
+    currentElement = PATH; // Para debug, por defecto no hay elemento seleccionado
+
+    updateDocElems();
 }
 
-function setupBoard() {
-    let board = new Array(DEF_COLS);
-    for(let i = 0; i < board.length; i++) {
-        board[i] = new Array(DEF_ROWS);
+function draw() {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            board[i][j].draw();
+        }
     }
 
-    for(let i = 0; i < board.length; i++) {
-        for(let j = 0; j < board[0].length; j++) {
+    if (mouseIsPressed) drawOnBoard();
+
+    updateDocElems()
+}
+
+function setupBoard(rows = DEF_ROWS, cols = DEF_COLS) {
+    let board = new Array(cols);
+    for (let i = 0; i < board.length; i++) {
+        board[i] = new Array(rows);
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
             board[i][j] = new Cell(i, j, DEF_SIZE);
         }
     }
@@ -39,12 +61,46 @@ function setupBoard() {
     return board;
 }
 
+function setupCanvas(cols = DEF_COLS, rows = DEF_ROWS) {
+    let canvas = createCanvas(DEF_SIZE * cols, DEF_SIZE * rows);
+    canvas.parent('canvas-board');
+    background(0);
+    board = setupBoard(rows, cols);
+}
 
-function draw() {
-    for(let i = 0; i < board.length; i++) {
-        for(let j = 0; j < board[0].length; j++) {
-            board[i][j].draw();
-        }
+// Actualiza todo lo que tiene que ver con el contenido de la página (contenido de las spans, h2, etc)
+function updateDocElems() {
+    document.getElementById('rows').innerText = rowSlider.value();
+    document.getElementById('columns').innerText = columnSlider.value();
+}
+
+function drawOnBoard() {
+    let { i, j } = getCoordinatesFromMouseCoordinates();
+    if (i == undefined || j == undefined) return;
+    if (currentElement) {
+        board[i][j].element = currentElement;
     }
+}
 
+function getCoordinatesFromMouseCoordinates() {
+    if (isMouseInTheBoard()) {
+        let i = Math.floor(mouseX / DEF_SIZE);
+        let j = Math.floor(mouseY / DEF_SIZE);
+
+        return { i, j }
+    }
+    else return { i: undefined, j: undefined };
+}
+
+function isMouseInTheBoard() {
+    return mouseX > 0 && mouseX < DEF_SIZE * board.length && mouseY > 0 && mouseY < DEF_SIZE * board[0].length;
+}
+
+// Funciones llamadas desde el html
+function changeSize() {
+    setupCanvas(columnSlider.value(), rowSlider.value());
+}
+
+function updateCurrentElement(element) {
+    currentElement = element;
 }
